b1cf0948e017e25a0e2fd66babc26ad166ebf156
Amelio Bergamin <arbergamin@gmail.com>
2020-04-22 16:24:58 -0400

    init add of conversion challenge

---
+++ day-19-challenge/Conversion/Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conversion
+//
+//  Created by Amelio Bergamin on 2020-04-22.
+//  Copyright © 2020 Amelio Bergamin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
